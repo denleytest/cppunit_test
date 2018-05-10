856a5cecfba552060e2999fdee6ce57fdd2ab558
Denley Hsiao <denley@justtodo.com>
2018-05-10 16:48:41 +0800

    Add CppUnitLite.

---
+++ CppUnitLite/main.cpp
@@ -0,0 +1,7 @@
+#include "CppUnitLite/TestHarness.h"
+
+int main(int argc, char* argv[])
+{
+	CommandLineTestRunner::RunAllTests(argc, argv);
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cppunit_test
+cpp各单元测试工具示例
